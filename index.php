740893e254d703e7a1d8f4029951ec06923fa8ba
Aryn Sampson <arynsampson0704@gmail.com>
2022-11-11 19:40:33 +0200

    starter index file with template files

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking App</title>
+</head>
+
+<body>
+
+    <?php require('templates/header.php'); ?>
+
+
+
+    <?php require('templates/form.php'); ?>
+
+
+
+    <?php require('templates/footer.php'); ?>
+
+</body>
+
+</html>
