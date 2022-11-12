f14c6f35f6ea14eaa1fc9c41ae1b7c053cfe5625
Aryn Sampson <arynsampson0704@gmail.com>
2022-11-12 18:58:07 +0200

    used POST variables to store input

--- input_validator.php
+++ input_validator.php
@@ -2,11 +2,6 @@
 
     class InputValidator {
         private $data;
-        private $firstname = '';
-        private $surname = '';
-        private $email = '';
-        private $hotel = '';
-        private $date = '';
         private $errors = array(
         'firstname' => '',
         'surname' => '',
@@ -25,39 +20,41 @@
         if(empty($this->data['firstname']) | strlen($this->data['firstname'] < 2)) {
             $this->errors['firstname'] = 'Please enter a valid name with at 2 characters.';
         } else {
-            $this->firstname = $this->data['firstname'];
+            $_POST['firstname'] = $this->data['firstname'];
         }
 
         // validate surname 
         if(empty($this->data['surname']) | strlen($this->data['surname'] < 2)) {
             $this->errors['surname'] = 'Please enter a valid surname with at 2 characters.';
         } else {
-            $this->surname = $this->data['surname'];
+            $_POST['surname'] = $this->data['surname'];
         }
 
         // validate email
         if(empty($this->data['email']) | strlen($this->data['email'] < 2)) {
             $this->errors['email'] = 'Please enter a valid email with at 2 characters.';
         } else {
-            $this->email = $this->data['email'];
+            $_POST['email'] = $this->data['email'];
         }
 
         // validate hotel
         if(empty($this->data['hotel'])) {
             $this->errors['hotel'] = 'Please pick a hotel.';
         } else {
-            $this->hotel = $this->data['hotel'];
+            $_POST['hotel'] = $this->data['hotel'];
         }
 
         // validate dates
         if(empty($this->data['checkInDate']) | empty($this->data['checkOutDate'])) {
             $this->errors['date'] = 'Please enter both dates.';
         } else {
-            if($this->data['checkInDate'] < $this->data['checkOutDate']) {
+            if($this->data['checkInDate'] > $this->data['checkOutDate']) {
                 $this->errors['date'] = 'Check in date should be before check out date.';
+            } else {
+                $_POST['checkInDate'] = $this->data['checkInDate'];
+                $_POST['checkOutDate'] = $this->data['checkOutDate'];
             }
         }
-
             return $this->errors;
         }
     }
