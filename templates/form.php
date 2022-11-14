81fcf1b0cac0165c0937b1f63e78b1095e3e50f0
Aryn Sampson <arynsampson0704@gmail.com>
2022-11-14 20:32:54 +0200

    added layout classes

--- templates/form.php
+++ templates/form.php
@@ -24,50 +24,53 @@
 ?>
 
 
-<div>
+<div class="content-container">
 
-    <h3>Hotel Reservations</h3>
+    <h1>Hotel <span id="heading-colour">Reservations</span></h1>
 
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-        <div>
-            <label for="firstname">First name:</label>
-            <input type="text" name="firstname" placeholder="Please enter your name" value="<?php echo htmlspecialchars($_POST['firstname'] ?? ''); ?>">
-            <p class="error"><?php echo $errors['firstname'] ?? ''; ?></p>
-        </div>
+        <div class="form-grid">
 
-        <div>
-            <label for="surname">Surname:</label>
-            <input type="text" name="surname" placeholder="Please enter your surname" value="<?php echo htmlspecialchars($_POST['surname'] ?? ''); ?>">
-            <p class="error"><?php echo $errors['surname'] ?? ''; ?></p>
-        </div>
+            <div>
+                <label for="firstname" class="form-label">First name:</label>
+                <input type="text" class="form-control" name="firstname" placeholder="Please enter your name" value="<?php echo htmlspecialchars($_POST['firstname'] ?? ''); ?>">
+                <p class="error"><?php echo $errors['firstname'] ?? ''; ?></p>
+            </div>
 
-        <div>
-            <label for="email">Email:</label>
-            <input type="email" name="email" placeholder="Please enter your email" value="<?php echo htmlspecialchars($_POST['email'] ?? ''); ?>">
-            <p class="error"><?php echo $errors['email'] ?? ''; ?></p>
-        </div>
+            <div>
+                <label for="surname" class="form-label">Surname:</label>
+                <input type="text" class="form-control" name="surname" placeholder="Please enter your surname" value="<?php echo htmlspecialchars($_POST['surname'] ?? ''); ?>">
+                <p class="error"><?php echo $errors['surname'] ?? ''; ?></p>
+            </div>
 
-        <div>
-            <label for="hotel">Select your hotel by clicking the dropdown menu below:</label>
-            <select name="hotel" id="hotel">
-                <option value=""></option>
-                <option value="One&Only">One&amp;Only</option>
-                <option value="The Commodore">The Commodore</option>
-            </select>
-            <p class="error"><?php echo $errors['hotel'] ?? ''; ?></p>
-        </div>
+            <div>
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" name="email" placeholder="Please enter your email" value="<?php echo htmlspecialchars($_POST['email'] ?? ''); ?>">
+                <p class="error"><?php echo $errors['email'] ?? ''; ?></p>
+            </div>
 
-        <div>
-            <label for="checkInDate">Check-in:</label>
-            <input type="date" name="checkInDate" value="<?php echo htmlspecialchars($_POST['checkInDate'] ?? ''); ?>">
-        </div>
+            <div>
+                <label for="hotel" class="form-label">Select your hotel:</label>
+                <select name="hotel" id="hotel" class="form-control">
+                    <option value=""></option>
+                    <option value="One&Only">One&amp;Only</option>
+                    <option value="The Commodore">The Commodore</option>
+                </select>
+                <p class="error"><?php echo $errors['hotel'] ?? ''; ?></p>
+            </div>
 
-        <div>
-            <label for="checkOutDate">Check-out:</label>
-            <input type="date" name="checkOutDate" value="<?php echo htmlspecialchars($_POST['checkOutDate'] ?? ''); ?>">
-        </div>
-        <p class="error"><?php echo $errors['date'] ?? ''; ?></p>
+            <div>
+                <label for="checkInDate" class="form-label">Check-in:</label>
+                <input type="date" class="form-control" name="checkInDate" value="<?php echo htmlspecialchars($_POST['checkInDate'] ?? ''); ?>">
+            </div>
 
-        <input type="submit" value="Submit" name="submit">
+            <div>
+                <label for="checkOutDate" class="form-label">Check-out:</label> 
+                <input type="date" class="form-control" name="checkOutDate" id="checkOutDate" value="<?php echo htmlspecialchars($_POST['checkOutDate'] ?? ''); ?>">
+            </div>
+                <p class="error"><?php echo $errors['date'] ?? ''; ?></p>
+            
+            <input type="submit" value="Submit" name="submit" class="btn btn-primary">
+        </div>
     </form>
 </div>
