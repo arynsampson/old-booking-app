1efe709ed7a6f893e7428054a6ecfe6dbd43b2ff
Aryn Sampson <arynsampson0704@gmail.com>
2022-11-17 20:35:12 +0200

    used formData to display inputs & errors

--- templates/form.php
+++ templates/form.php
@@ -2,24 +2,30 @@
 
     session_start();
 
-    require('input_validator.php');
+    require_once './utilities/input_validator.php';
 
     if (isset($_POST['submit'])) {
 
+        $data = array(
+                'firstname' => '',
+                'surname' => '',
+                'email' => '',
+                'hotel' => '',
+                'dates' => ''
+            );
         // validate user input
-        $validation = new InputValidator($_POST);
-        $errors = $validation->validateForm();
-        
+        $validation = new InputValidator($_POST, $data);
+        $formData = $validation->validateForm();
 
         // check if errors are present
-        if(!array_filter($errors)) {
+        if(!array_filter($formData[1])) {
             // assign input to SESSION
-            foreach($_POST as $key => $value) {
+            foreach($formData[0] as $key => $value) {
                 $_SESSION[$key] = $value;
             }
             // direct user to next screen
             header('Location: templates/compare.php');
-        }
+        } 
     }
 ?>
 
@@ -33,20 +39,20 @@
 
             <div>
                 <label for="firstname" class="form-label">First name:</label>
-                <input type="text" class="form-control" name="firstname" placeholder="Please enter your name" value="<?php echo htmlspecialchars($_POST['firstname'] ?? ''); ?>">
-                <p class="error"><?php echo $errors['firstname'] ?? ''; ?></p>
+                <input type="text" class="form-control" name="firstname" placeholder="Please enter your name" value="<?php echo htmlspecialchars($formData[0]['firstname'] ?? ''); ?>">
+                <p class="error"><?php echo $formData[1]['firstname'] ?? ''; ?></p>
             </div>
 
             <div>
                 <label for="surname" class="form-label">Surname:</label>
-                <input type="text" class="form-control" name="surname" placeholder="Please enter your surname" value="<?php echo htmlspecialchars($_POST['surname'] ?? ''); ?>">
-                <p class="error"><?php echo $errors['surname'] ?? ''; ?></p>
+                <input type="text" class="form-control" name="surname" placeholder="Please enter your surname" value="<?php echo htmlspecialchars($formData[0]['surname'] ?? ''); ?>">
+                <p class="error"><?php echo $formData[1]['surname'] ?? ''; ?></p>
             </div>
 
             <div>
                 <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" name="email" placeholder="Please enter your email" value="<?php echo htmlspecialchars($_POST['email'] ?? ''); ?>">
-                <p class="error"><?php echo $errors['email'] ?? ''; ?></p>
+                <input type="email" class="form-control" name="email" placeholder="Please enter your email" value="<?php echo htmlspecialchars($formData[0]['email'] ?? ''); ?>">
+                <p class="error"><?php echo $formData[1]['email'] ?? ''; ?></p>
             </div>
 
             <div>
@@ -56,19 +62,19 @@
                     <option value="One&Only">One&amp;Only</option>
                     <option value="The Commodore">The Commodore</option>
                 </select>
-                <p class="error"><?php echo $errors['hotel'] ?? ''; ?></p>
+                <p class="error"><?php echo $formData[1]['hotel'] ?? ''; ?></p>
             </div>
 
             <div>
                 <label for="checkInDate" class="form-label">Check-in:</label>
-                <input type="date" class="form-control" name="checkInDate" value="<?php echo htmlspecialchars($_POST['checkInDate'] ?? ''); ?>">
+                <input type="date" class="form-control" name="checkInDate" value="<?php echo htmlspecialchars($formData[0]['checkInDate'] ?? ''); ?>">
             </div>
 
             <div>
                 <label for="checkOutDate" class="form-label">Check-out:</label> 
-                <input type="date" class="form-control" name="checkOutDate" id="checkOutDate" value="<?php echo htmlspecialchars($_POST['checkOutDate'] ?? ''); ?>">
+                <input type="date" class="form-control" name="checkOutDate" id="checkOutDate" value="<?php echo htmlspecialchars($formData[0]['checkOutDate'] ?? ''); ?>">
             </div>
-                <p class="error"><?php echo $errors['date'] ?? ''; ?></p>
+                <p class="error"><?php echo $formData[1]['date'] ?? ''; ?></p>
             
             <input type="submit" value="Submit" name="submit" class="btn btn-primary">
         </div>
