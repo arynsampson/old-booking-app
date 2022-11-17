e5974fa02254c2d13fb0d91b693b57739b0e00fb
Aryn Sampson <arynsampson0704@gmail.com>
2022-11-17 20:34:06 +0200

    update style link & require

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Booking App</title>
 </head>
 
@@ -18,7 +14,7 @@
 
 
 
-    <?php require('templates/form.php'); ?>
+    <?php require_once 'templates/form.php'; ?>
 
 
 </body>
